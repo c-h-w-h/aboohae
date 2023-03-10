abe75704cde2e409e0d07f2ded112dd08d06e930
se030 <se030.kim@gmail.com>
2023-03-10 14:30:41 +0900

    feat: Routes 컴포넌트 구현
    - MobileContainer 적용
    - 기본 routes 설정
    
    Co-authored-by: Hyunbin Lee <iyu88@users.noreply.github.com>

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,10 +1,17 @@
+import MobileContainer from '@components-layout/MobileContainer';
 import { Routes as ReactRouterRoutes, Route } from 'react-router-dom';
+import BankPage from 'src/pages/BankPage';
 import IndexPage from 'src/pages/IndexPage';
+import LoanPage from 'src/pages/LoanPage';
 
 const Routes = () => {
   return (
     <ReactRouterRoutes>
-      <Route index element={<IndexPage />} />
+      <Route path="/" element={<MobileContainer />}>
+        <Route index element={<IndexPage />} />
+        <Route path="/loan" element={<LoanPage />} />
+        <Route path="/bank" element={<BankPage />} />
+      </Route>
     </ReactRouterRoutes>
   );
 };
