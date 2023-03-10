eff408d12524fa91a084eb8786f969d2ef6122cb
se030 <se030.kim@gmail.com>
2023-03-11 00:09:40 +0900

    feat: Header 컴포넌트 수정
    - Index navigator 추가
    - linkStyle 추가

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -11,31 +11,32 @@ const Header = () => {
 
   const { pathname } = useLocation();
 
-  const containerStyle = css`
-    width: 100%;
-    padding: 16px;
-    box-shadow: 0 1px 5px rgba(0, 0, 0, 0.2);
-  `;
+  const { color } = useTheme();
+  const { black } = color;
 
   return (
     <Flexbox as="header" css={containerStyle} justifyContent="space-between">
-      <Typography variant={'subtitle1'}>🏦 아부해</Typography>
+      <Link to="/" css={linkStyle}>
+        <Typography variant={'subtitle1'} color={black}>
+          🏦 아부해
+        </Typography>
+      </Link>
       <Flexbox as="nav">
-        <Link to="/">
+        <Link to="/" css={linkStyle}>
           <Icon
             source={MdHome}
             size={24}
             color={pathname === '/' ? primary100 : gray100}
           />
         </Link>
-        <Link to="/bank">
+        <Link to="/bank" css={linkStyle}>
           <Icon
             source={MdList}
             size={24}
             color={pathname === '/bank' ? primary100 : gray100}
           />
         </Link>
-        <Link to="/loan">
+        <Link to="/loan" css={linkStyle}>
           <Icon
             source={MdAdd}
             size={24}
@@ -46,5 +47,19 @@ const Header = () => {
     </Flexbox>
   );
 };
+
+const containerStyle = css`
+  width: 100%;
+  padding: 16px;
+  box-shadow: 0 1px 5px rgba(0, 0, 0, 0.2);
+`;
+
+const linkStyle = css`
+  text-decoration: none;
+
+  :hover {
+    pointer: cursor;
+  }
+`;
 
 export default Header;
