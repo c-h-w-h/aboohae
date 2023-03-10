97b4611860fda7ff7bfe02c2aa6d80f99b752fb7
se030 <se030.kim@gmail.com>
2023-03-10 17:26:19 +0900

    feat: LoanForm에서 사용할 Checkbox 컴포넌트 추가

---
+++ src/components/LoanForm/Checkbox.tsx
@@ -0,0 +1,27 @@
+import styled from '@emotion/styled';
+import { ReactNode, useState } from 'react';
+
+interface CheckboxProps {
+  id: string;
+  label: ReactNode;
+}
+
+const Checkbox = ({ id, label }: CheckboxProps) => {
+  const [isChecked, setIsChecked] = useState(false);
+  const onClick = () => setIsChecked((prev) => !prev);
+
+  return (
+    <>
+      <label htmlFor={id} onClick={onClick}>
+        {label}
+      </label>
+      <InvisibleInput type="checkbox" defaultChecked={isChecked} />
+    </>
+  );
+};
+
+const InvisibleInput = styled.input`
+  display: none;
+`;
+
+export default Checkbox;
