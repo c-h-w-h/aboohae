0f91c5428da3662beb1d278b3b2841f1fa1ea7c7
iyu88 <iyu88@naver.com>
2023-03-10 20:39:29 +0900

    feat: BankPage 기본 UI 레이아웃 설정
    더미 데이터로 기본 레이아웃이 렌더링되는 모습을 확인합니다.
    TabMenu는 임의로 Button 컴포넌트를 사용하여 표시합니다.

--- src/pages/BankPage.tsx
+++ src/pages/BankPage.tsx
@@ -1,5 +1,83 @@
+import Badge from '@components/Badge';
+import Button from '@components/Button';
+import Checkbox from '@components/Checkbox';
+import Spacing from '@components/Spacing';
+import Flexbox from '@components-layout/Flexbox';
+import { CATEGORY } from '@constants/category';
+import { Fragment } from 'react';
+
 const BankPage = () => {
-  return <div>BankPage</div>;
+  const tabMenu = ['전체', '부채', '적금', '상환'];
+
+  return (
+    <div>
+      <Flexbox justifyContent={'start'}>
+        {tabMenu.map((menu) => (
+          <Button key={menu} text={menu} />
+        ))}
+      </Flexbox>
+      <Spacing size={15} />
+      <Flexbox justifyContent={'start'}>
+        {Object.values(CATEGORY).map((c) => (
+          <Badge key={c}>{c}</Badge>
+        ))}
+      </Flexbox>
+      <Spacing size={15} />
+      <Flexbox flexDirection="column">
+        {DUMMY_ARTICLES.map((article) => {
+          const { id, title, readAt } = article;
+          return (
+            <Fragment key={id}>
+              <Checkbox
+                value={id.toString()}
+                label={title}
+                checked={readAt && true}
+                onChange={(_) => null}
+              />
+              <Spacing size={15} />
+            </Fragment>
+          );
+        })}
+      </Flexbox>
+    </div>
+  );
 };
 
 export default BankPage;
+
+const DUMMY_ARTICLES: Article[] = [
+  {
+    id: 1,
+    link: 'www.naver.com',
+    title:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore',
+    categories: ['JavaScript', 'TypeScript'],
+    bookmark: false,
+    readAt: new Date(),
+  },
+  {
+    id: 2,
+    link: 'www.naver.com',
+    title:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore',
+    categories: ['DesignSystem'],
+    bookmark: false,
+    readAt: new Date(),
+  },
+  {
+    id: 3,
+    link: 'www.naver.com',
+    title:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore',
+    categories: ['JavaScript', 'TypeScript'],
+    bookmark: false,
+  },
+  {
+    id: 4,
+    link: 'www.naver.com',
+    title:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore',
+    categories: [],
+    bookmark: false,
+  },
+];
