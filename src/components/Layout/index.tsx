5a85a56ca71a6396cf77e3a2b3006837c38f8c74
dohun <dohun31@naver.com>
2023-03-10 22:12:23 +0900

    fix: Layout의 padding 더 크게 변경

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -2,12 +2,13 @@ import Header from '@components/Header';
 import Spacing from '@components/Spacing';
 import Container from '@components-layout/Container';
 import MobileContainer from '@components-layout/MobileContainer';
+import { SPACING } from '@constants/spacing';
 import { css } from '@emotion/react';
 import { Outlet } from 'react-router-dom';
 
 const Layout = () => {
   const bodyStyle = css`
-    padding: 0 16px;
+    padding: 0 ${SPACING[60]};
   `;
 
   return (
